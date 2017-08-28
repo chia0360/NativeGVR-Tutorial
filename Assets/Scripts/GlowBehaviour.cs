e9185314ca5e5acd726f1919dbd9e51e89c283a0
chia0360 <chia0360@e.ntu.edu.sg>
2017-08-28 22:39:51 +0800

    Added Main Scenes and other Assets

---
+++ Assets/Scripts/GlowBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowBehaviour : MonoBehaviour {
+
+    public Material HighlightMaterial;
+    private Material normalMaterial;
+    private Renderer myRenderer;
+	// Use this for initialization
+	void Start () {
+        this.myRenderer = GetComponent<Renderer>();
+        this.normalMaterial = this.myRenderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Highlight() {
+        myRenderer.material = HighlightMaterial;
+    }
+
+    public void NormalRendering() {
+        myRenderer.material = normalMaterial;
+    }
+}
---
+++ Assets/Scripts/VRItemEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using VRStandardAssets.Utils;
+
+[RequireComponent(typeof(VRInteractiveItem))]
+public class VRItemEventHandler : MonoBehaviour {
+
+    private VRInteractiveItem item;
+    public UnityEvent GazeEnterEvent;
+    public UnityEvent GazeExitEvent;
+
+	// Use this for initialization
+	void Start () {
+        item = GetComponent<VRInteractiveItem> ();
+        item.OnOver += OnGazeEnter;
+        item.OnOut += OnGazeExit;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGazeEnter() {
+        GazeEnterEvent.Invoke ();
+    }
+
+    void OnGazeExit() {
+        GazeExitEvent.Invoke ();
+    }
+}
